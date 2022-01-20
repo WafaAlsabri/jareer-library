5c60ed7a14dcb349c711139ae21d7b883e249b3b
WafaAlsabri <wafaalsabri23@gmail.com>
2022-01-20 16:04:16 +0300

    preee

--- assets/js/p.js
+++ assets/js/p.js
@@ -1,12 +1,108 @@
 
+var data=0;
+document.getElementById("root").innerText=data;
+
+function increment(){data=data+1;
+document.getElementById("root").innerText=data;}
+
+
+
+
+
+function search_animal() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('cv');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
+/*document.querySelector(".plus-btn").setAttribute("disabled","disabled");
+var valuCounter
+
+
+document.querySelector(".plus-btn").setAttribute("click",function(){
+  var valuCounter=document.getElementById("root").value;
+  valuCounter++;
+  document.getElementById("root").value=valuCounter;
+
+  if(valuCounter>1){
+    document.querySelector(".plus-btn").setAttribute("disabled","disabled")
+  
+  }
+
+     //setting default attribute to disabled of minus button
+     document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
+
+     //taking value to increment decrement input value
+     var valueCount
+
+     //taking price value in variable
+     var price = document.getElementById("price").innerText;
+
+     //price calculation function
+     function priceTotal() {
+         var total = valueCount * price;
+         document.getElementById("price").innerText = total
+     }
+
+     //plus button
+     document.querySelector(".plus-btn").addEventListener("click", function() {
+         //getting value of input
+         valueCount = document.getElementById("quantity").value;
+
+         //input value increment by 1
+         valueCount++;
+
+         //setting increment input value
+         document.getElementById("quantity").value = valueCount;
+
+         if (valueCount > 1) {
+             document.querySelector(".minus-btn").removeAttribute("disabled");
+             document.querySelector(".minus-btn").classList.remove("disabled")
+         }
+
+         //calling price function
+         priceTotal()
+     })
+
+     //plus button
+     document.querySelector(".minus-btn").addEventListener("click", function() {
+         //getting value of input
+         valueCount = document.getElementById("quantity").value;
+
+         //input value increment by 1
+         valueCount--;
+
+         //setting increment input value
+         document.getElementById("quantity").value = valueCount
+
+         if (valueCount == 1) {
+             document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+         }
+
+         //calling price function
+         priceTotal()
+     })
+
+
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+   
   }
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+   
+
+ 
 
   function openForm2() {
     document.getElementById("myForm1").style.display = "block";
